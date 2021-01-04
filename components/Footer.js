7107d79c9b95b9a0ba205831a6ce38bb90c7f45b
mariliab <marilia.bognandi@gmail.com>
2021-01-04 13:48:22 +0100

    add changes to footer and navbar

--- components/Footer.js
+++ components/Footer.js
@@ -1,46 +1,159 @@
 import Link from "next/link";
 import styled from "styled-components";
+import Head from "next/head";
 
-const Title = styled.h2`
-  color: white;
+const Container = styled.div`
+  padding: 2rem 1rem 3rem 1rem;
+  background: rgba(0, 0, 0, 0.8);
+  min-height: 300px;
 `;
-const Email = styled.a`
+
+const Heading = styled.h2`
+  margin-top: 0;
+  margin-bottom: 2rem;
   color: white;
-  font-size: 150%;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-size: 1rem;
+  color: #ddcdc1;
+`;
+const StyledLink = styled.a`
+  color: white;
+  font-size: 2rem;
   text-decoration: none;
+  font-weight: 200;
   @media (min-width: 768px) {
-    font-size: 300%;
+    font-size: 3rem;
   }
   :hover {
     color: #ddcdc1;
   }
+  :not(:last-child) {
+    margin-bottom: 1rem;
+  }
 `;
 
-const BuiltWithContainer = styled.div`
+const Copyright = styled.p`
   margin-top: 3rem;
+  margin-bottom: 0;
   color: #ddcdc1;
-  display: flex;
-  font-size: 75%;
-  font-style: italic;
+  text-align: center;
+  width: 100%;
+  font-size: 12px;
 `;
 
-const Container = styled.div`
-  background: rgba(0, 0, 0, 0.8);
-  min-height: 300px;
+const Text = styled.p`
+  margin: 0;
+  color: rgba(254, 254, 254, 0.7);
+  line-height: 1.4;
+`;
+
+const SocialLinks = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+`;
+
+const LinkWrapper = styled.div`
+  > a {
+    color: white;
+    :hover {
+      color: #ddcdc1;
+    }
+  }
+  :not(:last-child) {
+    @media (max-width: 991px) {
+      margin-bottom: 1.5rem;
+    }
+    @media (min-width: 992px) {
+      margin-bottom: 1.5rem;
+      margin-right: 1rem;
+    }
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1200px;
+  margin: 0 auto;
+  @media (max-width: 991px) {
+    flex-direction: column;
+  }
+`;
+const Column = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Logo = styled.div`
+  font-family: "Playfair Display", serif;
+  font-weight: bold;
+  font-size: 1.5rem;
+  cursor: pointer;
+  color: #ddcdc1;
+  margin-bottom: 1rem;
+
+  > span {
+    font-weight: 300;
+    font-family: "Josefin Sans", sans-serif;
+    color: white;
+  }
+  @media (min-width: 992px) {
+    font-size: 1.75rem;
+  }
 `;
 
 export default function Footer() {
   return (
     <Container>
-      <Title>Get in touch</Title>
-      <Email href="mailto:marilia@morpheus.se">marilia@morpheus.se</Email>
-      <BuiltWithContainer>
-        Built with Next.js, React.js, Contentful CMS, deployed on Vercel.
-      </BuiltWithContainer>
+      <Row>
+        <Column>
+          <Heading>Get in touch</Heading>
+          <StyledLink href="mailto:marilia@morpheus.se">
+            marilia@morpheus.se
+          </StyledLink>
+          <StyledLink>+46736937742</StyledLink>
+        </Column>
+        <Column>
+          <Heading>Social</Heading>
+          <SocialLinks>
+            <LinkWrapper>
+              <a
+                href="https://www.linkedin.com/in/mariliabognandi/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </LinkWrapper>
+            <LinkWrapper>
+              <a
+                href="https://www.instagram.com/bognandiphotography/"
+                target="_blank"
+              >
+                Instagram
+              </a>
+            </LinkWrapper>
+            <LinkWrapper>
+              <a href="https://github.com/mariliab" target="_blank">
+                Github
+              </a>
+            </LinkWrapper>
+          </SocialLinks>
+        </Column>
+        <Column>
+          <Logo>
+            MB <span>portfolio</span>
+          </Logo>
+          <Text>
+            Built with Next.js, React.js, Contentful CMS, <br></br>deployed on
+            Vercel.
+          </Text>
+        </Column>
+      </Row>
+      <Row>
+        <Copyright>Copyright © Marilia Bognandi 2021</Copyright>
+      </Row>
     </Container>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -24,6 +24,7 @@ const HeaderWrapper = styled.header`
 const DesktopHeader = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   @media (max-width: 991px) {
     display: none;
   }
@@ -63,12 +64,43 @@ const NavItemGroup = styled.div`
 
 const LinkWrapper = styled.div`
   @media (max-width: 991px) {
-    margin-bottom: 1rem;
-    font-size: 200%;
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
   }
   @media (min-width: 992px) {
     margin-left: 1rem;
+    font-size: 1.25rem;
   }
+`;
+
+const Logo = styled.div`
+  font-family: "Playfair Display", serif;
+  font-weight: bold;
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  > span {
+    font-weight: 300;
+    font-family: "Josefin Sans", sans-serif;
+    color: rgba(0, 0, 0, 0.7);
+  }
+  @media (min-width: 992px) {
+    font-size: 1.75rem;
+  }
+`;
+
+const MobileLogo = styled(Logo)`
+  position: absolute;
+  top: 1rem;
+`;
+
+const MobileCategoryHeading = styled.h2`
+  font-family: "Playfair Display", serif;
+  font-size: 1rem;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #9a8f87;
+  letter-spacing: 1px;
 `;
 
 const Navbar = ({ data }) => {
@@ -78,12 +110,16 @@ const Navbar = ({ data }) => {
     <>
       <HeaderWrapper id="header">
         <DesktopHeader>
-          <Link href="/">Marilia Bognandi</Link>
+          <Link href="/">
+            <Logo>
+              MB<span> portfolio</span>
+            </Logo>
+          </Link>
           <NavItemGroup>
             <LinkWrapper
               className={router.asPath == "/portfolio" ? "active" : ""}
             >
-              <Link href="/portfolio">Portfolio</Link>
+              <Link href="/portfolio">Projects</Link>
             </LinkWrapper>
             <LinkWrapper className={router.asPath == "/about" ? "active" : ""}>
               <Link href="/about">About</Link>
@@ -95,16 +131,28 @@ const Navbar = ({ data }) => {
         </DesktopHeader>
         <MobileHeader>
           <MobileHeaderContainer>
-            <Link href="/">Marilia Bognandi</Link>
+            <Link href="/">
+              <Logo>
+                M.B<span> portfolio</span>
+              </Logo>
+            </Link>
             <BurgerMenu open={open} setOpen={setOpen} />
           </MobileHeaderContainer>
           <MobileMenuBody open={open} setOpen={setOpen}>
+            <MobileLogo>
+              <Link href="/">
+                <Logo>
+                  MB<span> portfolio</span>
+                </Logo>
+              </Link>
+            </MobileLogo>
             <NavItemGroup>
+              <MobileCategoryHeading>Content</MobileCategoryHeading>
               <LinkWrapper
                 className={router.asPath == "/portfolio" ? "active" : ""}
                 onClick={() => setOpen(!open)}
               >
-                <Link href="/portfolio">Portfolio</Link>
+                <Link href="/portfolio">Projects</Link>
               </LinkWrapper>
               <LinkWrapper
                 className={router.asPath == "/about" ? "active" : ""}
@@ -114,6 +162,30 @@ const Navbar = ({ data }) => {
               </LinkWrapper>
               <LinkWrapper onClick={() => setOpen(!open)}>
                 <a href="mailto:marilia@morpheus.se">Contact</a>
+              </LinkWrapper>
+            </NavItemGroup>
+            <NavItemGroup>
+              <MobileCategoryHeading>Social</MobileCategoryHeading>
+              <LinkWrapper>
+                <a
+                  href="https://www.linkedin.com/in/mariliabognandi/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </LinkWrapper>
+              <LinkWrapper>
+                <a
+                  href="https://www.instagram.com/bognandiphotography/"
+                  target="_blank"
+                >
+                  Instagram
+                </a>
+              </LinkWrapper>
+              <LinkWrapper>
+                <a href="https://github.com/mariliab" target="_blank">
+                  Github
+                </a>
               </LinkWrapper>
             </NavItemGroup>
           </MobileMenuBody>
