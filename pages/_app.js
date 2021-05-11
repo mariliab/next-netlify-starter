dcd884fa152caadef63665ff81a8985496e56d44
mariliab <marilia.bognandi@gmail.com>
2021-05-11 20:14:51 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,26 @@ import { useSpring, animated } from "react-spring";
 import { GlobalStyles } from "../components/GlobalStyles";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
 
 function Application({ Component, pageProps }) {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <animated.div style={props}>
